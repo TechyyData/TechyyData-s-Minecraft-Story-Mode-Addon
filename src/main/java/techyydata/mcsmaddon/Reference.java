d865fecf5febeb416224607f09be0d792b42a82b
TechyyData <34737526+TechyyData@users.noreply.github.com>
2018-01-05 23:44:09 -0600

    Source

---
+++ src/main/java/techyydata/mcsmaddon/Reference.java
@@ -0,0 +1,10 @@
+package techyydata.mcsmaddon;
+
+public class Reference {
+
+    public static final String MOD_ID = "mcsmaddon";
+    public static final String NAME = "TechyyData's Minecraft Story Mode Addon";
+    public static final String VERSION = "1.0";
+
+    public static final String CLIENT_PROXY_CLASS = "";
+}
